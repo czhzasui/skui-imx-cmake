5e93626cbd8a7379ba9393d0c875b98cf5998dba
Ruben Van Boxem <vanboxem.ruben@gmail.com>
2019-09-28 15:57:55 +0200

    prevent double inclusion of toolchain_options.cmake

--- toolchain_options.cmake
+++ toolchain_options.cmake
@@ -22,6 +22,8 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 # THE SOFTWARE.
 
+include_guard(GLOBAL)
+
 # Compiler options
 if(CMAKE_COMPILER_IS_GNUCXX)
   set(CMAKE_CXX_FLAGS "-pedantic-errors -Wextra -Wconversion -Winit-self -Wmissing-include-dirs -Wstrict-aliasing -Werror ${CMAKE_CXX_FLAGS}")
